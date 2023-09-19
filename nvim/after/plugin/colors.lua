bf9683cec90dcf9e87479b181f11acbb3fd2e6eb
Daniel Rodríguez <daniel@dpstudios.es>
2023-09-20 00:23:50 +0200

    feat: tired of transparent bg

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,10 +3,10 @@ function SetColor(color)
     vim.cmd.colorscheme(color)
     vim.cmd.highlight("iCursor guifg=black guibg=white")
     vim.opt.guicursor = "v-n-c:block-Cursor,i:block-Cursor"
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
 end
 
 SetColor()
