6814759ec7a35819c100ace02b04200b89412cb9
Daniel Rodríguez <daniel@dpstudios.es>
2023-09-20 00:22:45 +0200

    feat: using input mono

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+rm ~/.tmux.conf || true
 ln -s $PWD/tmux.conf $HOME/.tmux.conf 
+rm -r ~/.config/alacritty || true
+mkdir ~/.config/alacritty
 ln -s $PWD/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+rm -r ~/.config/nvim || true
 ln -s $PWD/nvim $HOME/.config/nvim
