1514ebfe48fd8da49229feed0398727a1a18111d
Daniel Rodriguez <drodriguez@newrelic.com>
2024-04-03 12:38:28 +0200

    feat: add pane shortcut to wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -5,5 +5,18 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Batman'
 config.font = wezterm.font 'CaskaydiaCove Nerd Font'
 config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    key = 'd',
+    mods = 'SHIFT|CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  }
+}
 
 return config
