330de660745d1bad24c125a12cf225deb6778376
Daniel Rodriguez <drodriguez@newrelic.com>
2024-04-04 22:11:11 +0200

    feat: update font and so

--- wezterm.lua
+++ wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font 'MartianMono Nerd Font'
-config.font_size = 12
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.font_size = 14
 config.line_height = 1.2
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
@@ -24,7 +24,7 @@ config.keys = {
     action = wezterm.action.PaneSelect
   },
 }
-config.initial_rows = 32
-config.initial_cols = 100
+config.initial_rows = 36
+config.initial_cols = 120
 
 return config
