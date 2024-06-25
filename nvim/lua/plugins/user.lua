b4432333b5e991e2df623487629a59f86ccd5151
Daniel Rodriguez Gil <daniel@dpstudios.es>
2024-06-25 02:02:42 +0200

    feat: wezterm

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -3,9 +3,6 @@
 
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
   "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
@@ -36,51 +33,6 @@ return {
       return opts
     end,
   },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
-
   -- == PHP ==
   {
     "nvim-treesitter/nvim-treesitter",
@@ -132,19 +84,7 @@ return {
     },
   },
   {
-    "vimwiki/vimwiki",
-    init = function()
-        print("Setting up vimwiki!")
-        vim.g.vimwiki_list = {
-            {
-                template_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/vimwiki/autoload/",
-                syntax = "markdown",
-                ext = ".md",
-                path = "~/Documents/Wiki",
-            },
-        }
-        vim.g.vimwiki_global_ext = 0
-    end,
-},
-
+    "mrjones2014/smart-splits.nvim",
+    event = "BufRead", -- load on very lazy for mux detection
+  }
 }
--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,39 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
-config.color_scheme = 'rose-pine-dawn'
+local function is_vim(pane)
+  return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+local direction_keys = {
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
+config.color_scheme = 'rose-pine-moon'
 config.font = wezterm.font('GeistMono Nerd Font', { weight = 400 })
 config.font_size = 13
 config.line_height = 1.2
@@ -15,7 +46,49 @@ config.window_padding = {
 	top = 4,
 	bottom = 4,
 }
+-- Leader is the same as my old tmux prefix
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
+  -- splitting
+  {
+    mods   = "LEADER",
+    key    = "-",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods   = "LEADER",
+    key    = "=",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods = 'LEADER',
+    key = 'm',
+    action = wezterm.action.TogglePaneZoomState
+  },
+  {
+    mods = "LEADER",
+    key = "Space",
+    action = wezterm.action.RotatePanes "Clockwise"
+  },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    mods = 'LEADER',
+    key = '0',
+    action = wezterm.action.PaneSelect {
+      mode = 'SwapWithActive',
+    },
+  },
+  -- nvim
+  -- move between split panes
+  split_nav('move', 'h'),
+  split_nav('move', 'j'),
+  split_nav('move', 'k'),
+  split_nav('move', 'l'),
+  -- resize panes
+  split_nav('resize', 'h'),
+  split_nav('resize', 'j'),
+  split_nav('resize', 'k'),
+  split_nav('resize', 'l'),
 	{
 		key = "d",
 		mods = "CMD",
@@ -38,4 +111,6 @@ config.initial_cols = 140
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
+
 return config
+
